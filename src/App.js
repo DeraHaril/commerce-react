d9be60b6620cd8dd1a7119399af43846dfb08933
Dera <deraharilala@gmail.com>
2023-04-19 11:55:20 +0300

    adding main files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React, { Component} from 'react';
+import Homepage from './homepage.component';
+
+function App(){
+  return(
+    <Homepage />
+  )
+} 
+
+export default App;
