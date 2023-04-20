e77675b4770ac60f1355b87b2d039d28a7b54e84
Dera <deraharilala@gmail.com>
2023-04-20 15:25:06 +0300

    update menu-item adding navigation with all menus adapted with react v6(useNavigate instead history.push)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
-import React, { Component} from 'react';
+import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
+import { Routes, Route, useParams, Link } from 'react-router-dom';
+
+const Hats = (props) => {
+  console.log(props);
+  return(
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
+const Sneakers = () => {
+  return(
+  <div>
+    <Link to='/'><h1>Sneakers</h1></Link>
+    <span>{}</span>
+  </div>    
+  )
+}
 
 function App(){
   return(
-    <Homepage />
+    <Routes>
+      <Route path='/' Component={Homepage}/>
+      <Route path='/hats' Component={Hats}/>
+      <Route path='/sneakers' Component={Sneakers}/>
+    </Routes>
   )
 } 
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import './menu-item.style.scss';
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl}) => {
+    const navigate = useNavigate();
+    return(
+    <div    className={`${size} menu-item`} 
+            onClick={() => navigate(`/${linkUrl}`)}>
         <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop now</span>
         </div>
     </div>
-)
+    )
+}
 
 export default MenuItem;
