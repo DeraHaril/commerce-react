897260aea9aeba74fad2bec6b3a106d7eb35240c
Dera <deraharilala@gmail.com>
2023-04-26 11:30:46 +0300

    updating app with a header component and Links

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import { Routes, Route, useParams, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 function App(){
   return(
-    <Routes>
-      <Route path='/' Component={Homepage}/>
-      <Route path='/shop' Component={ShopPage}/>
-    </Routes>
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='/' Component={Homepage}/>
+        <Route path='/shop' Component={ShopPage}/>
+      </Routes>      
+    </div>
+
   )
 } 
 
